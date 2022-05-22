72dbda9d42c631569064f9531b3649d087faed46
Julia <at.julia@gmail.com>
2022-05-22 22:01:02 +0300

    Catch exception

--- gui.py
+++ gui.py
@@ -7,5 +7,8 @@ root = tk.Tk()
 root.withdraw()
 
 file_path = filedialog.askopenfilename()
-run(file_path)
+try:
+    run(file_path)
+except BaseException as err:
+    print(err)
 input("Press Enter to exit...")
